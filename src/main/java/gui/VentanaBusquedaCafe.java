2916cfa6b1123a1662c50b512ede214658589c8a
ignacio487 <i.riquelme10@ufromail.cl>
2023-10-12 01:36:16 -0300

    caso cafeteria

--- src/main/java/gui/VentanaBusquedaCafe.java
+++ src/main/java/gui/VentanaBusquedaCafe.java
@@ -1,8 +1,16 @@
 package gui;
 
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class VentanaBusquedaCafe {
+    Connection con;
+    DefaultListModel mod = new DefaultListModel();
+    Statement statement;
+    ResultSet resultSet;
     private JComboBox comboBox1;
     private JComboBox comboBox2;
     private JButton buscarButton;
@@ -14,5 +22,24 @@ public class VentanaBusquedaCafe {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void buscarCafe(){
+        conectar();
+        String tipo = comboBox1.getSelectedItem().toString();
+        String precio = comboBox2.getSelectedItem().toString();
+        String sql = "SELECT * FROM cafe WHERE tipo = '"+tipo+"' AND precio = '"+precio+"'";
+        try{
+            resultSet = statement.executeQuery(sql);
+            while(resultSet.next()){
+                mod.addElement(resultSet.getString(2));
+            }
+            JOptionPane.showMessageDialog(null, "Cafe encontrado");
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Error al buscar cafe");
+        }
+    }
+
+    private void conectar() {
     }
 }
