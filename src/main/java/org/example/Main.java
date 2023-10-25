98404939bd530808cfe83a78582fda39ae8e88ae
ignacio487 <i.riquelme10@ufromail.cl>
2023-10-25 19:13:18 -0300

    caso cafeteria

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,19 +9,15 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // Crear una instancia de Cafeteria
         Cafeteria cafeteria = new Cafeteria();
 
-        // Crear una instancia de CafeteriaControlador
         CafeteriaControlador controlador = new CafeteriaControlador(cafeteria);
 
-        // Agregar algunos cafés a la cafetería
         Cafe cafe1 = new Cafe("Cafe1", "Grande", "Información Café 1");
         Cafe cafe2 = new Cafe("Cafe2", "Mediano", "Información Café 2");
         cafeteria.agregarCafe(cafe1);
         cafeteria.agregarCafe(cafe2);
 
-        // Obtener un café por nombre
         Cafe cafeObtenido = controlador.obtenerCafe("Cafe1");
 
         if (cafeObtenido != null) {
@@ -30,13 +26,11 @@ public class Main {
             System.out.println("Café no encontrado");
         }
 
-        // Modificar la información de un café
         if (cafeObtenido != null) {
             controlador.modificarInformacion(cafeObtenido, "Nueva información del café");
             System.out.println("Información del café modificada.");
         }
 
-        // Obtener cafés por tamaño
         ArrayList<Cafe> cafesGrandes = controlador.obtenerCafesPorTamaño("Grande");
         System.out.println("Cafés Grandes:");
         for (Cafe cafe : cafesGrandes) {
