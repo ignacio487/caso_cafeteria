ccbe85b5af20f64929fcfb18cf792bde25c705c4
ignacio487 <i.riquelme10@ufromail.cl>
2023-10-25 15:10:45 -0300

    caso cafeteria

--- src/main/java/gui/VentanaDescontinuarCafe.java
+++ src/main/java/gui/VentanaDescontinuarCafe.java
@@ -1,9 +1,26 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VentanaDescontinuarCafe {
     private JButton descontinuarCafeButton;
     private JPanel panel1;
     private JButton regresarButton;
+
+    public VentanaDescontinuarCafe() {
+        regresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        descontinuarCafeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
--- src/main/java/gui/VentanaModificarInformacion.java
+++ src/main/java/gui/VentanaModificarInformacion.java
@@ -1,6 +1,8 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VentanaModificarInformacion {
     private JButton nombreCafeteriaButton;
@@ -8,4 +10,37 @@ public class VentanaModificarInformacion {
     private JButton preciosButton;
     private JButton modificarButton;
     private JButton regresarButton;
+
+    public VentanaModificarInformacion() {
+        nombreCafeteriaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        stockButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        preciosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        modificarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        regresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
