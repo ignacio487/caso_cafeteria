c5dc7218bbd0adc18d14a0c7265a262d23a7c236
ignacio487 <i.riquelme10@ufromail.cl>
2023-10-25 15:04:06 -0300

    caso cafeteria

---
+++ src/main/java/gui/VentanaDescontinuarCafe.java
@@ -0,0 +1,9 @@
+package gui;
+
+import javax.swing.*;
+
+public class VentanaDescontinuarCafe {
+    private JButton descontinuarCafeButton;
+    private JPanel panel1;
+    private JButton regresarButton;
+}
---
+++ src/main/java/gui/VentanaModificarInformacion.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.*;
+
+public class VentanaModificarInformacion {
+    private JButton nombreCafeteriaButton;
+    private JButton stockButton;
+    private JButton preciosButton;
+    private JButton modificarButton;
+    private JButton regresarButton;
+}
